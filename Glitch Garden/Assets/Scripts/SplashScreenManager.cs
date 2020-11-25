6a5401b8baa7889816f8a95a03cd7061b4f519f0
Mateusz Kaczmarkiewicz <matt327k@gmail.com>
2020-11-25 09:06:17 +0100

    pierwszy commit

---
+++ Glitch Garden/Assets/Scripts/SplashScreenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadingScreen());
+    }
+
+    public IEnumerator LoadingScreen()
+    {
+        AudioSource myAudioSource = GetComponent<AudioSource>();
+        myAudioSource.PlayOneShot(myAudioSource.clip);
+
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("Start Scene");
+    }
+}
