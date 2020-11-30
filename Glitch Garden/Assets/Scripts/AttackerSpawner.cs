a09d07a208f3d218f0e06af0a3d93ad22ec00e6f
Mateusz Kaczmarkiewicz <matt327k@gmail.com>
2020-11-30 18:51:19 +0100

    wykrywanie przeciwnikow w linii

--- Glitch Garden/Assets/Scripts/AttackerSpawner.cs
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -12,7 +12,7 @@ public class AttackerSpawner : MonoBehaviour
     {
         while(spawn)
         {
-            yield return new WaitForSeconds(Random.Range(1, 5));
+            yield return new WaitForSeconds(Random.Range(12, 12));
 
             SpawnAttacker();
         }
--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -7,6 +7,48 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject zuchciniGun;
 
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else 
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isInTheSameLane = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon + 0.1;
+            //to 0.1 jest takim rozwiązaniem na trytytkę, bo ten skrypt znajduje się na dziecku "Body" które jest przesunięte względem rodzica o 0.1
+            if (isInTheSameLane)
+            {
+                myLaneSpawner = spawner;
+                break;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        return myLaneSpawner.transform.childCount > 0;
+    }
+
     public void Fire()
     {
         Instantiate(projectile, zuchciniGun.transform.position, zuchciniGun.transform.rotation);
