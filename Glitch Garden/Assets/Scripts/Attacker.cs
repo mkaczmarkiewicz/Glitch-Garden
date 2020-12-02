516e70b75bace5b5c1e9c10a3c95ffb878fb4ff4
Mateusz Kaczmarkiewicz <matt327k@gmail.com>
2020-12-02 21:58:48 +0100

    dodana mechanika i animacja ataku jaszczurki

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -5,14 +5,47 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     float speed = 0f;
-    
+
+    GameObject currentTarget;
+
+    Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * speed);
+
+        if (myAnimator.GetBool("isAttacking"))
+        {
+            Attack();
+        }
     }
 
     public void SetSpeed(float newSpeed)
     {
         speed = newSpeed;
+    }
+
+    public void Attack()
+    {
+        //atakuj currentTarget;
+    }
+
+    public void StopAttacking()
+    {
+        myAnimator.SetBool("isAttacking", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {    
+        if (collision.gameObject.tag == "Defender")
+        {
+            currentTarget = collision.gameObject;
+            myAnimator.SetBool("isAttacking", true);
+        }
     }
 }
